126c981b9b5194e8c6a4023ed8a7de112f705239
MOHAMED ASHIK RAHMAN R <ashikrahmanr24@gmail.com>
2026-08-23 14:19:37 +0530

    Time: 1 ms (99.58%), Space: 47.1 MB (73.04%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] sol = new int[nums.length];
+        for(int i=0;i<nums.length;i++) {
+            sol[i]=nums[nums[i]];
+        }
+        return sol;
+    }
+}
