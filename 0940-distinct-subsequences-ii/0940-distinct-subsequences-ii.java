575d27a49640fd6fd0faefcb5da31db5789dc8e9
MOHAMED ASHIK RAHMAN R <ashikrahmanr24@gmail.com>
2026-09-15 19:24:23 +0530

    Time: 5 ms (31.51%), Space: 43.4 MB (23.52%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,17 +1,21 @@
 class Solution {
     public int distinctSubseqII(String s) {
         int MOD = 1_000_000_007;
-        int ans = 0;
-        int[] lastCount = new int[26];
-        char[] chars = s.toCharArray();      
-        for (char c : chars) {
-            int idx = c - 'a';
-            int currentAdded = (ans + 1 - lastCount[idx] + MOD) % MOD;
-            int nextAns = (ans + currentAdded) % MOD;
-            lastCount[idx] = (ans + 1) % MOD;         
-            ans = nextAns;
-        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        int[] lastPosition = new int[26];
         
-        return ans;
+        for (int i = 1; i <= n; i++) {
+            char ch = s.charAt(i - 1);
+            int charIndex = ch - 'a';
+            dp[i] = (dp[i - 1] * 2) % MOD;
+            if (lastPosition[charIndex] > 0) {
+                int prevIdx = lastPosition[charIndex];
+                dp[i] = (dp[i] - dp[prevIdx - 1] + MOD) % MOD;
+            }
+            lastPosition[charIndex] = i;
+        }
+        return (dp[n] - 1 + MOD) % MOD;
     }
 }
